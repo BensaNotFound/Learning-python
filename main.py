4f22a2a27fba25a16f7fe090aa893066a3d77301
BensaNotFound <loonilaine@gmail.com>
2025-09-15 08:38:16 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Hello, Welcome to Bensa´s coffee")
+
+name = input("What is your name?\n")
+
+print("Hello " + name + ", Thank you so much for coming in today.\n\n\n")
+
+menu = "Coffee, Espresso, Capuccino, Latte, Ice Coffee"
+
+print(name + ", what would you like from our menu today? Here is what we are serving.\n" + menu)
+
+order = input()
+
+print("Excellent choice! " + name + ", We will have that " + order + " ready for you in a moment.")
